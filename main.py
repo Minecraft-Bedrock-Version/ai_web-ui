2555f0fccfd803220d6386bf6214ed667e981218
Juhyeok0603 <juhyeok20603@gmail.com>
2026-01-18 14:08:33 +0900

    modify-1

--- main.py
+++ main.py
@@ -12,4 +12,4 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.get("/", response_class=HTMLResponse) 
 async def root():
-    return templates.TemplateResponse("cloud.html", {"request": {}})
+    return templates.TemplateResponse("index.html", {"request": {}})
